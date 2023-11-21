0d1b86f9f11b50346a813cac2fa4794d6eb56183
Iqra562 <iqra44938@gmail.com>
2023-11-21 16:33:23 +0500

    login

--- CardProject/CardProject/Controllers/AdminController.cs
+++ CardProject/CardProject/Controllers/AdminController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CardProject.Models;
+using DotNetConnection.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CardProject.Controllers
 {
     public class AdminController : Controller
     {
-      
+        private readonly ApplicationDbContext _db;
+        public AdminController(ApplicationDbContext db)
+        {
+           _db = db;
+        }
         public IActionResult Login()
         {
+            return View();
+
+
+        }
+        [HttpPost]
+        public IActionResult Login(Register r )
+        {
+            var login = _db.Registers.Where(a => a.email == r.email && a.password == r.password).FirstOrDefault();
+           if (login != null) {
+                ViewBag.data = "loign";
+            }
+            else
+            {
+                ViewBag.data = "invalid";
+            }
             return View();
 
 
