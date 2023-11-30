406a7e92bdc576a5f25fd379a215f90c07928b34
sajid <bilalsajid532@gmail.com>
2023-11-30 16:29:02 +0500

    categoryadded

---
+++ CardProject/CardProject/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using CardProject.Models;
+using DotNetConnection.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CardProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            ViewBag.category = new SelectList(_db.Categories, "CategoryId", "CategoryName");
+            ViewBag.brand = new SelectList(_db.Brands, "BrandId", "BrandName");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            
+            return View();
+        }
+    }
+}
