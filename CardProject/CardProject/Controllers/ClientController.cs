f71493ac8dde1966a83eccfe6672bbe942acb021
sajid <bilalsajid532@gmail.com>
2023-12-16 16:20:20 +0500

    categoryLoop

---
+++ CardProject/CardProject/Controllers/ClientController.cs
@@ -0,0 +1,20 @@
+﻿using DotNetConnection.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardProject.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ClientController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var products = _db.Products.Skip(1).Take(5).ToList();
+
+            return View(products);
+        }
+    }
+}
